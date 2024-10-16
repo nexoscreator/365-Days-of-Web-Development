15439ff8671d04cf37514adb086d1e92021e2de6
nexoscreator <nexoscreator@gmail.com>
2024-10-16 08:29:43 +0530

    Day: 02 Added

---
+++ Day 02/main.js
@@ -0,0 +1,18 @@
+// Toast Notification
+function showToast(message, type = 'info') {
+    const toast = document.createElement('div');
+    toast.className = `toast ${type}`;
+    toast.innerHTML = `<span class="toast-icon"></span> ${message}`;
+
+    document.getElementById('toastContainer').appendChild(toast);
+    setTimeout(() => {
+        toast.classList.add('show');
+    }, 100);
+
+    setTimeout(() => {
+        toast.classList.remove('show');
+        setTimeout(() => {
+            toast.remove();
+        }, 300);
+    }, 3000);
+}
